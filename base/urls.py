c04487c97e3860a5cbd33343f193ebf47da21f44
joza <joza@localhost.localdomain>
2023-10-18 10:40:15 +0000

    mise

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name='base'
 urlpatterns = [
     
-    path('', views.meal_list, name='meal_list'),
-    path('index/', views.index, name='index'),
+    path('shop/', views.meal_list, name='meal_list'),
+    path('', views.index, name='index'),
     path('meals/<int:meal_id>/', views.meal_detail, name='meal_detail'),
     path('add_to_cart/<int:meal_id>/<int:quantity>', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<int:meal_id>/', views.remove_from_cart, name='remove_from_cart'),
