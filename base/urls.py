f1eeabdc9ee7426dd9e879c82db8244bcb026881
joza <joza@localhost.localdomain>
2023-10-20 14:32:49 +0000

    about

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     
     path('shop/', views.meal_list, name='meal_list'),
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('meals/<int:meal_id>/', views.meal_detail, name='meal_detail'),
     path('add_to_cart/<int:meal_id>/<int:quantity>', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<int:meal_id>/', views.remove_from_cart, name='remove_from_cart'),
